95b198e68896940004c57da9fddf99afd0ab0d99
Jose Gonzalez <josgonzalez@kippla.org>
2019-09-19 08:37:15 -0700

    lesson 2

--- Intro to Vue.js/main.js
+++ Intro to Vue.js/main.js
@@ -2,6 +2,6 @@ var app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
-    description: 'a garment for the foot and lower part of the leg, typically knitted from wool, cotton, or nylon'
+    image: './assets/vmSocks-green-onWhite.jpg'
   }
 })
