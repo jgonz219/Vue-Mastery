560a007b4fc66c24c91c4b9c091822ed41e17665
Jose Gonzalez <josgonzalez@kippla.org>
2019-09-18 19:00:20 -0700

    lesson 1

---
+++ Intro to Vue.js/main.js
@@ -0,0 +1,7 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    product: 'Socks',
+    description: 'a garment for the foot and lower part of the leg, typically knitted from wool, cotton, or nylon'
+  }
+})
