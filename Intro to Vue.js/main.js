fae016529d40ef8181340c90652ddef60a637d56
Jose Gonzalez <jgonz219@gmail.com>
2019-09-23 21:45:39 -0700

    lesson 5

--- Intro to Vue.js/main.js
+++ Intro to Vue.js/main.js
@@ -2,6 +2,39 @@ var app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
-    image: './assets/vmSocks-green-onWhite.jpg'
+    image: './assets/vmSocks-green-onWhite.jpg',
+    inventory: 100,
+    inStock: true,
+    onSale: false,
+    details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+    sizes: ['S', 'M', 'L', 'XL'],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: 'green',
+        variantImage: './assets/vmSocks-green-onWhite.jpg'
+      },
+      {
+        variantId: 2235,
+        variantColor: 'blue',
+        variantImage: './assets/vmSocks-blue-onWhite.jpg'
+      }
+    ],
+    cart: 0
+  },
+
+  methods: {
+    addToCart: function() {
+      this.cart += 1
+
+    },
+    removeFromCart: function() {
+      if (this.cart > 0) {
+        this.cart -= 1
+      }
+    },
+    updateProduct(variantImage) {
+      this.image = variantImage
+    }
   }
 })
